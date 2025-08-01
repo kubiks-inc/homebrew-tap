0d616f2a2c90df16700e94b93c0a6df7c13d865d
Alex Holovach <alex.holovach@kubiks.ai>
2025-07-31 21:11:51 -0500

    update

--- Formula/kubiks.rb
+++ Formula/kubiks.rb
@@ -5,10 +5,10 @@ class Kubiks < Formula
   
   # Version 1.0.8
   if Hardware::CPU.arm?
-    url "https://github.com/kubiks-inc/tap/releases/download/v1.0.8/kubiks-darwin-arm64.tar.gz"
+    url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v1.0.8/kubiks-v1.0.8-darwin-arm64.tar.gz"
     sha256 "ba6e72f47ae7ae8a3f4972dd560080e98425950277b115fd24ae056e022e4548"
   else
-    url "https://github.com/kubiks-inc/tap/releases/download/v1.0.8/kubiks-darwin-amd64.tar.gz"
+    url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v1.0.8/kubiks-v1.0.8-darwin-amd64.tar.gz"
     sha256 "fb1b06aa4f9aa1dc7c9b8c306bf7615250cf3fb60a3c368b136116eb604f20f9"
   end
 
