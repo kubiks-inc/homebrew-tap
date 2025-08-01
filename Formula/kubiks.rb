669db9bb19020b21a1d421ceeb2c9f5149416c0c
Alex Holovach <alex.holovach@kubiks.ai>
2025-07-31 22:02:32 -0500

    update

--- Formula/kubiks.rb
+++ Formula/kubiks.rb
@@ -4,13 +4,8 @@ class Kubiks < Formula
   version "1.0.9"
   license "Apache-2.0"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v1.0.9/kubiks-v1.0.9-darwin-arm64.tar.gz"
-    sha256 "a062402151d7a251190f67a30e0ce8f464668c752ccf8d6dfa483640db07bc55"
-  else
-    url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v1.0.9/kubiks-v1.0.9-darwin-amd64.tar.gz"
-    sha256 "6ca2b1bf3f300488f45942da8c7afce91dd7c00da7e43c9d46f22eec89317ab0"
-  end
+  url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v1.0.13/kubiks-v1.0.13-darwin-amd64.tar.gz"
+  sha256 "d0beb2fdbee4c498485eceff08888541480d037156a212f8e021cf9330b7eb4b"
 
   def install
     bin.install "kubiks"
