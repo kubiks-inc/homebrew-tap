1cd033267e994d2d85a1b088d99f30f4c5dae58d
Alex Holovach <alex.holovach@kubiks.ai>
2025-07-31 21:27:59 -0500

    Add version field to formula

--- Formula/kubiks.rb
+++ Formula/kubiks.rb
@@ -1,9 +1,9 @@
 class Kubiks < Formula
   desc "AI-powered debugging for Next.js with OpenTelemetry and MCP integration"
   homepage "https://github.com/kubiks-inc/kubiks-cli"
+  version "1.0.8"
   license "Apache-2.0"
 
-  # Version 1.0.8
   if Hardware::CPU.arm?
     url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v1.0.8/kubiks-v1.0.8-darwin-arm64.tar.gz"
     sha256 "ba6e72f47ae7ae8a3f4972dd560080e98425950277b115fd24ae056e022e4548"
