37f93c3f6c506578fe00a695228581a7e2f51fdd
Alex Holovach <alex.holovach@kubiks.ai>
2025-08-15 23:01:22 -0500

    2.0.4

--- Formula/kubiks.rb
+++ Formula/kubiks.rb
@@ -1,9 +1,9 @@
 class Kubiks < Formula
   desc "AI-powered debugging for Next.js with OpenTelemetry and MCP integration"
   homepage "https://github.com/kubiks-inc/kubiks-cli"
-  url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v2.0.3/kubiks-v2.0.3-darwin-amd64.tar.gz"
-  version "2.0.3"
-  sha256 "cd9d31f011405cf49072e6e78d043a60e197ec2cc8865ebd99b486d8d99a8914"
+  url "https://github.com/kubiks-inc/kubiks-cli/releases/download/v2.0.4/kubiks-v2.0.4-darwin-amd64.tar.gz"
+  version "2.0.4"
+  sha256 "d0856cd88680cd363215427d912627ad916050a140eb80e9390b53b12528f5f3"
   license "Apache-2.0"
 
   def install
